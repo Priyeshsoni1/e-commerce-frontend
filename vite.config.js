3986625a3c61ab8686802e63645ec523ee313cfe
Priyesh Soni <priyesh9792@gmail.com>
2025-04-01 00:43:45 +0530

    remove port

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from "tailwindcss";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(import.meta.env.VITE_PORT) || 3000, // Use Render's PORT env variable or fallback to 3000
     host: "0.0.0.0", // Bind to all network interfaces for external access
   },
   css: {
